a6d506e5be9fafc5a99219413536040d377b13a5
samuelmuthembwa <70405740+samuelmuthembwa@users.noreply.github.com>
2022-02-27 14:43:42 +0300

    Alita Botfirst Update 11

--- index.js
+++ index.js
@@ -363,6 +363,9 @@ const startSock = async() => {
                     help();
 
                 break;
+                case "!league":
+                    premier();
+                break;
                 case "!contact":
                     contact();
                 break;
@@ -384,43 +387,7 @@ const startSock = async() => {
                     contact()
                 break;
                 case "alivebtnPremier":
-                    try {
-                        let clubs = []
-                        let pts = []
-                        let club_info = []
-                        let data = "\n「「  👸🏾 *Alita Bot* 💚❤️ 」」\n\n ⚽ *Premier League* ⚽\n_______________________\n";
-                        let url = "https://www.sportsmole.co.uk/football/live-scores/";
-                        axios({
-                            method: 'get',
-                            url: url
-                        })
-                        .then((res)=>{
-                            let $ = cheerio.load(res.data);
-                            // Scrappring games
-                            $(".leaguetable .s1").find('a:nth-child(1)').each(function() {
-                                clubs.push($(this).text().trim());
-                            });
-                            //Scapping PTS
-                            $(".leaguetable .positiontd").find('strong').each(function() {
-                                pts.push($(this).text().trim());
-                            });
-                            for(var i = 0; i<clubs.length ; i++)
-                            {
-                                club_info.push({name: clubs[i], pts: pts[i]})
-                            }
-                            for(var i = 0; i<club_info.length ; i++)
-                            {
-                                data = data+"⚽ "+"*"+club_info[i].name+"*"+" PTS :"+"*"+club_info[i].pts+"*"+"\n";
-                            }
-                            sock.sendMessage(m.key.remoteJid, {text: data})
-                        })
-                        .catch(()=>{
-                            sock.sendMessage(m.key.remoteJid, {text: "「「  👸🏾 *Alita Bot* 💚❤️ 」」\n\n 😒 Couldn't Fetch Premier League Data. "})
-                        })
-                        
-                    } catch (error) {
-                        sock.sendMessage(m.key.remoteJid, {text: "「「  👸🏾 *Alita Bot* 💚❤️ 」」\n\n 😒 Couldn't Fetch Premier League Data. "})
-                    }
+                    premier();
                 break
                 case "alivebtnhelp":
                     help();
@@ -541,6 +508,47 @@ const startSock = async() => {
             }
 
 
+        }
+
+        async function premier()
+        {
+            try {
+                let clubs = []
+                let pts = []
+                let club_info = []
+                let data = "\n「「  👸🏾 *Alita Bot* 💚❤️ 」」\n\n ⚽ *Premier League* ⚽\n_______________________\n";
+                let url = "https://www.sportsmole.co.uk/football/live-scores/";
+                axios({
+                    method: 'get',
+                    url: url
+                })
+                .then((res)=>{
+                    let $ = cheerio.load(res.data);
+                    // Scrappring games
+                    $(".leaguetable .s1").find('a:nth-child(1)').each(function() {
+                        clubs.push($(this).text().trim());
+                    });
+                    //Scapping PTS
+                    $(".leaguetable .positiontd").find('strong').each(function() {
+                        pts.push($(this).text().trim());
+                    });
+                    for(var i = 0; i<clubs.length ; i++)
+                    {
+                        club_info.push({name: clubs[i], pts: pts[i]})
+                    }
+                    for(var i = 0; i<club_info.length ; i++)
+                    {
+                        data = data+"⚽ "+"*"+club_info[i].name+"*"+" PTS :"+"*"+club_info[i].pts+"*"+"\n";
+                    }
+                    sock.sendMessage(m.key.remoteJid, {text: data})
+                })
+                .catch(()=>{
+                    sock.sendMessage(m.key.remoteJid, {text: "「「  👸🏾 *Alita Bot* 💚❤️ 」」\n\n 😒 Couldn't Fetch Premier League Data. "})
+                })
+                
+            } catch (error) {
+                sock.sendMessage(m.key.remoteJid, {text: "「「  👸🏾 *Alita Bot* 💚❤️ 」」\n\n 😒 Couldn't Fetch Premier League Data. "})
+            }
         }
 
 
